26fe1a40a8c9ce2d0e77516a17f1d08d89c9739b
adriano.stewart <adriano.stewart@accenture.com>
2017-02-15 10:01:42 +0100

    Pulled sending message logic into own function in seperate class

--- src/index.ts
+++ src/index.ts
@@ -82,40 +82,7 @@ const client = new Wit({accessToken, actions});
 
 var authUser:auth.Authentication = new auth.Authentication("mibi","mibi");
 authUser.authenticate(io);
-
-io.on('connection', function(socket) {
-    console.log('User Connected');
-    socket.on('message', function(msg){
-
-        io.emit('message', msg);
-        console.log('got message', msg)
-
-        client.runActions(
-            sessionId, // the user's current session
-            msg.text, // the user's message
-            context // the user's current session state
-        ).then((newContext) => {
-            // Our bot did everything it has to do.
-            // Now it's waiting for further messages to proceed.
-            console.log('Waiting for next user messages');
-
-            // Based on the session state, you might want to reset the session.
-            // This depends heavily on the business logic of your bot.
-            // Example:
-            // if (context['done']) {
-            //   delete sessions[sessionId];
-            // }
-
-            // Updating the user's current session state
-            context = newContext;
-        })
-            .catch((err) => {
-                console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
+authUser.sendMessage(io, client,sessionId, context);
 
 
-        console.log(msg);
-
-    });
-});
 
--- src/security/Authentication.ts
+++ src/security/Authentication.ts
@@ -23,5 +23,43 @@ export class Authentication {
             }
         });
     }
+
+    public sendMessage(io, client, sessionId, context){
+        io.on('connection', function(socket) {
+            console.log('User Connected');
+            socket.on('message', function(msg){
+
+                io.emit('message', msg);
+                console.log('got message', msg)
+
+                client.runActions(
+                    sessionId, // the user's current session
+                    msg.text, // the user's message
+                    context // the user's current session state
+                ).then((newContext) => {
+                    // Our bot did everything it has to do.
+                    // Now it's waiting for further messages to proceed.
+                    console.log('Waiting for next user messages');
+
+                    // Based on the session state, you might want to reset the session.
+                    // This depends heavily on the business logic of your bot.
+                    // Example:
+                    // if (context['done']) {
+                    //   delete sessions[sessionId];
+                    // }
+
+                    // Updating the user's current session state
+                    context = newContext;
+                })
+                    .catch((err) => {
+                        console.error('Oops! Got an error from Wit: ', err.stack || err);
+                    })
+
+
+                console.log(msg);
+
+            });
+        });
+    }
 }
 
