a739a977ba7405c532392c5687be1e995aef2432
adriano.stewart <adriano.stewart@accenture.com>
2017-04-11 10:00:19 +0200

    Fixed a bunch of tests

--- test/db/MiBiFirebase.test.ts
+++ test/db/MiBiFirebase.test.ts
@@ -1,124 +1,111 @@
 import * as mocha from 'mocha';
 import chaiHttp = require('chai-http');
 let chai = require('chai');
-import {FirebaseDatabaseReader} from '../../src/db/FirebaseDatabaseReader';
 import {fbDbReader} from "../../src/index";
 
-let db = fbDbReader;
 let chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 let expect = chai.expect;
 
-let company = 'Din Begravelse';
+let company = 'Lekebutikken';
 let phone = 41639898;
 let puk = 456812;
 let name = 'line johnsen';
-let subscription = {
-    additionalProducts: [{
-        endDate: '2017-04-01T00:00:00.000Z',
-        productName: 'DataPakke 2GB'
-    }],
-    companyName: 'Din Begravelse',
-    dataDefault: 3000,
-    dataNotified: false,
-    dataTotal: 5000,
-    dataUsed: 4384,
-    name: 'john abrahamsen',
-    priceDefault: 299,
-    priceTotal: 499,
-    puk: 456812
-}
-let subscriptions = {
-    '41639898': {
-        additionalProducts: [[Object]],
-        companyName: 'Din Begravelse',
-        dataDefault: 3000,
-        dataNotified: false,
-        dataTotal: 5000,
-        dataUsed: 4384,
-        name: 'john abrahamsen',
-        priceDefault: 299,
-        priceTotal: 499,
-        puk: 456812
-    },
-    '91454568': {
-        additionalProducts: [[Object]],
-        companyName: 'Din Begravelse',
-        dataDefault: 5000,
-        dataNotified: false,
-        dataTotal: 10000,
-        dataUsed: 7460,
-        name: 'tone fjellheim',
-        priceDefault: 349,
-        priceTotal: 599,
-        puk: 124979
-    },
-    '94685512': {
-        companyName: 'Din Begravelse',
-        dataDefault: 5000,
-        dataNotified: false,
-        dataTotal: 5000,
-        dataUsed: 2312,
-        name: 'line johnsen',
-        priceDefault: 349,
-        priceTotal: 349,
-        puk: 689461
-    }
-}
-let admin_name = 'mibi';
-let admin = {
-        companyName: 'Din Begravelse',
-        name: 'line johnsen',
-        password: 'mibi',
-        uuid: '342c5180-d9ab-4419-9c96-ddf907f3558b'
-}
-let admins = {
-    mibi: {
-        companyName: 'Din Begravelse',
-        name: 'line johnsen',
-        password: 'mibi',
-        uuid: '342c5180-d9ab-4419-9c96-ddf907f3558b'
-    },
-    mobo: {
-        companyName: 'Din Begravelse',
-        name: 'john abrahamsen',
-        password: 'mobo',
-        uuid: 'fd2148be-fe6c-487e-a5f6-1fbd219defd7'
-    }
-}
-
+let adminName = 'mibi';
+let path = '/admins/mibi/name';
+let adminEmail = 'line.johnsen@lekebutikken.no';
 
 describe('Getting subscriptions', () => {
-        it('Should return subscription for company and be equal to expected values', () => {
-            return expect(db.getSubscription(company, phone)).to.eventually.deep.equal(subscription);
+    it('Should return subscription for company and be equal to expected values', () => {
+        fbDbReader.getSubscription(company, phone).then((subscription) => {
+            expect(subscription).length.to.equal(1);
         });
-        it('Should return all subscriptions for a company and be equal to expected values', () => {
-            return expect(db.getSubscriptions(company)).to.eventually.contain.keys(['94685512', '91454568', '41639898']);
+
+    });
+    it('Should return all subscriptions for a company and be equal to expected values', () => {
+        fbDbReader.getSubscriptions(company).then((subscriptions) => {
+            expect(subscriptions).length.to.equal(3);
         });
-    }
-)
-;
+    });
+});
 
 describe('Getting puk', () => {
     it('Should return the puk of the requested user', () => {
-        return expect(db.getPuk(company, phone)).to.eventually.equal(puk);
+        fbDbReader.getPuk(company, phone).then((puk) => {
+            expect(puk).to.equal(puk);
+        });
+    });
+    it('Should return the promise',(done) => {
+        expect(fbDbReader.getPuk(company, phone)).to.eventually.be.fulfilled.and.notify(done);
     });
 });
 
 describe('Getting numbers for person', () => {
     it('Should return all numbers for a person', () => {
-        return expect(db.getNumbers(company, name)).to.eventually.have.key(name);
+        fbDbReader.getNumbers(company, name).then((numbers) => {
+            expect(numbers).length.to.equal(1);
+        });
     });
     it('Should return all numbers for a person with an incomplete name', () => {
-        return expect(db.getNumbers(company, name.substring(0, 6))).to.eventually.have.key(name);
+        fbDbReader.getNumbers(company, name.substring(0,6)).then((numbers) => {
+            expect(numbers).length.to.equal(1);
+        });
+    });
+    it('Should return the promise',(done) => {
+        expect(fbDbReader.getNumbers(company, name)).to.eventually.be.fulfilled.and.notify(done);
     });
 });
 
-describe('Getting admins for company', () => {
+describe('Getting admins', () => {
     it('Should return all admins for a company', () => {
-        return expect(db.getAdminsForCompany(company)).to.eventually.deep.equal(admins);
+        fbDbReader.getAdminsForCompany(company).then((admins) => {
+            expect(admins).length.to.equal(2);
+        });
     });
     it('Should return a single admin', () => {
-        return expect(db.getAdmin(admin_name)).to.eventually.deep.equal(admin);
+        fbDbReader.getAdmin(adminName).then((admin) => {
+            expect(admin).length.to.equal(1);
+        });
+    });
+    it('Should return all admins', () => {
+        fbDbReader.getAdmins().then((admins) => {
+            expect(admins).length.to.equal(3);
+        });
     });
 });
+
+describe('Getting specific data', () => {
+    it('Should return the exact data requested', () => {
+        fbDbReader.getSpecificPath(path).then((data) => {
+            expect(data).to.equal(name);
+        });
+    });
+});
+
+describe('Getting email for admin', () => {
+    it('Should return the email address of an admin', () => {
+        fbDbReader.getEmail(adminName).then((email) => {
+            expect(email).to.equal(adminEmail);
+        });
+    });
+});
+
+describe('Getting updates', () => {
+    it('Should not return null object for single subscription', () => {
+        fbDbReader.getUpdate(phone).then((update) => {
+            expect(update).to.not.equal(null);
+        });
+    });
+    it('Should not return null object for all updates', () => {
+        fbDbReader.getUpdates().then((updates) => {
+            expect(updates).to.not.equal(null);
+        });
+    });
+});
+
+describe('Getting device tokens', () => {
+    it('Should return all tokens for an admin', () => {
+
+    });
+});
+
