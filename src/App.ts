19f59d6f101328a973b166e3378c7f86fadc671b
adriano.stewart <adriano.stewart@accenture.com>
2017-04-11 16:17:46 +0200

    removed unused import

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 import {NotificationRouter} from "./routes/NotificationRouter";
-import {UserRouter} from "./routes/UserRouter";
+// import {UserRouter} from "./routes/UserRouter";
 
 
 // Creates and configures an ExpressJS web server.
