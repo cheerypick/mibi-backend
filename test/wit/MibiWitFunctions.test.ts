b0447ca06b4eb70d93f95d96ce88a66d0aa6e04b
adriano.stewart <adriano.stewart@accenture.com>
2017-04-12 10:00:46 +0200

    Added more tests

--- test/wit/MibiWitFunctions.test.ts
+++ test/wit/MibiWitFunctions.test.ts
@@ -8,6 +8,19 @@ let chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 let expect = chai.expect;
 
+let subscription = {
+    name: "Some Name",
+    puk: 123456,
+    dataUsed: 1000,
+    dataTotal: 2000
+}
+let entities = {
+    number: {
+        0: {
+            value: 90605040
+        }
+    }
+}
 
 describe('Creating responses', () => {
     it('Should return number not found response', () => {
@@ -16,8 +29,61 @@ describe('Creating responses', () => {
         expect(response).to.not.have.key('quickreplies');
     });
     it('Should return numbers found response', () => {
-        let response = MibiWitFunctions.createNumbersFound([90605040, 40605090]);
+        let response = MibiWitFunctions.createNumbersFoundResponse([90605040, 40605090]);
         expect(response).to.contain.key('text');
         expect(response).to.contain.key('quickreplies');
+    });
+});
+
+describe('Creating contexts', () => {
+    it('Should return a puk context', () => {
+        let context = {};
+        // let subscription = {
+        //     name: "Some Name",
+        //     puk: 123456,
+        //     dataUsed: 1000,
+        //     dataTotal: 2000
+        // }
+        // let entities = {
+        //     number: {
+        //         0: {
+        //             value: 90605040
+        //         }
+        //     }
+        // }
+        let response = MibiWitFunctions.createPukContext(context, subscription, entities);
+        expect(response).to.contain.keys(['puk', 'name', 'number']);
+        expect(response.puk).to.equal(subscription.puk);
+        expect(response.name).to.equal(subscription.name);
+        expect(response.number).to.equal(entities.number[0].value);
+    });
+    it('Should return an update context', () => {
+        let context = {};
+        let data = {
+
+        };
+        let socket = {
+            _userInfo: {
+                username: "username"
+            }
+        };
+        let date = new Date();
+        let daysLeft = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() - date.getDate();
+
+        let response = MibiWitFunctions.createUpdatesContext(context, socket, subscription, entities);
+        expect(response).to.have.keys(['subscription', 'used', 'total', 'days', 'admin']);
+        expect(response.subscription).to.equal(subscription.name);
+        expect(response.used).to.equal(subscription.dataUsed);
+        expect(response.total).to.equal(subscription.dataTotal);
+        expect(response.days).to.equal(daysLeft);
+        expect(response.admin).to.equal(socket._userInfo.username);
+
+        expect(socket).to.have.keys(['_userInfo', '_updateData']);
+    });
+    it('Should return a joke context', () => {
+        let context = {}
+
+        let response = MibiWitFunctions.createJokeContext(context);
+        expect(response).to.have.key('joke');
     });
 });
