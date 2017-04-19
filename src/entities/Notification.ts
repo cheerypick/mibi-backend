90bc1466fe3cbb137a8f487c697b91fed8eef55e
adriano.stewart <adriano.stewart@accenture.com>
2017-04-19 10:57:17 +0200

    push bug again....

--- src/entities/Notification.ts
+++ src/entities/Notification.ts
@@ -3,13 +3,12 @@ export class Notification{
     title:string;
     body:string;
     sound:string = "default";
-    message:string;
+    icon:string = "myicon";
     witAction:string;
 
     constructor(title:string, body:string, witAction:string){
         this.title = title;
         this.body = body;
-        this.message = body;
         this.witAction = witAction;
     }
 
