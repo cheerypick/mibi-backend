02f4a2766581ff509d83748eb981fc0a7d3558b1
Hans Fredrik Brastad <hansfredrik@outlook.com>
2017-04-20 13:49:24 +0200

    Adding badge as payload for notification.

--- src/entities/Notification.ts
+++ src/entities/Notification.ts
@@ -5,6 +5,7 @@ export class Notification{
     sound:string = "default";
     witAction:string;
     click_action:String = "FCM_PLUGIN_ACTIVITY";
+    badge = 1;
 
     constructor(title:string, body:string, witAction:string){
         this.title = title;
